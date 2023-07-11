d9790eae1d42c5765e52180a99a08ffff13c0278
Onuki <onuki@mist.i.u-tokyo.ac.jp>
2023-07-11 16:06:58 +0900

    simplify FromSpecialProdToDim2

--- d2isogeny.py
+++ d2isogeny.py
@@ -78,9 +78,9 @@ def FromProdToDim2(P1, Q1, P2, Q2, e):
         assert T2.is_zero() or T2 == S2
         phi1 = E1.isogeny(T1)
         phi2 = E2.isogeny(S2)
-        def isogeny(Ps):
-            P1, P2 = Ps
-            return phi1(P1), phi2(P2)
+        def isogeny(Rs):
+            R1, R2 = Rs
+            return phi1(R1), phi2(R2)
         imP = isogeny([P1, P2])
         imQ = isogeny([Q1, Q2])
         return [phi1.codomain(), phi2.codomain()], imP[0], imP[1], imQ[0], imQ[1], isogeny
@@ -88,80 +88,65 @@ def FromProdToDim2(P1, Q1, P2, Q2, e):
         return richelot.FromProdToJac(E1, E2, P1, Q1, P2, Q2, e)
 
 # (2,2)-isogeny from the elliptic curve defined by y^2 = x^3 + x
-def FromSpecialProdToDim2(T0, T1, S0, S1, e):
-    E = T0.curve()
-    assert E == T1.curve() == S0.curve() == S1.curve()
+def FromSpecialProdToDim2(P1, Q1, P2, Q2, e):
+    E = P1.curve()
+    assert E == P2.curve() == Q1.curve() == Q2.curve()
     assert E == EllipticCurve(E.base_ring(), [1, 0])
 
     F = E.base_ring()
     Rx = PolynomialRing(F, name="x")
     x = Rx.gens()[0]
 
-    T20 = 2**(e-1)*T0
-    T21 = 2**(e-1)*T1
-    S20 = 2**(e-1)*S0
-    S21 = 2**(e-1)*S1
+    T1 = 2**(e-1)*P1
+    T2 = 2**(e-1)*P2
+    S1 = 2**(e-1)*Q1
+    S2 = 2**(e-1)*Q2
 
     # the product of 2-isogenies from E
-    if T20.is_zero() or T21.is_zero() or S20.is_zero() or S21.is_zero():
-        if T20.is_zero() or T21.is_zero():
-            if T20.is_zero():
-                T20, T21 = T21, T20
-                S20, S21 = S21, S20
-            assert not T20.is_zero()
-            phi1 = E.isogeny(T20)
-            phi2 = E.isogeny(S21)
-            assert S20 == S21 or S20.is_zero()
-            def isogeny(Ps):
-                P1, P2 = Ps
-                return phi1(P1), phi2(P2)
+    if T1.is_zero() or T2.is_zero() or S1.is_zero() or S2.is_zero():
+        print("hoge")
+        h, _, _, _, _, phi = FromProdToDim2(T1, S1, T2, S2, 1)
+        imP = phi([P1, P2])
+        imQ = phi([Q1, Q2])
+        return h, imP[0], imP[1], imQ[0], imQ[1], phi
+
+    # change T1 to (0, 0) in E
+    if S1[0] == 0:
+        T1, S1 = S1, T1
+        T2, S2 = S2, T2
+    elif not T1[0] == 0:
+        T1 += S1
+        T2 += S2
+    assert T1[0] == 0
+
+    if T2[0] == 0:
+        if S1 == S2:
+            # kernel is {(R, R) | R in E[2]}
+            def isogeny(Rs):
+                R1, R2 = Rs
+                return R1 - R2, R1 + R2
         else:
-            if S20.is_zero():
-                T20, T21 = T21, T20
-                S20, S21 = S21, S20
-            assert not S20.is_zero()
-            phi1 = E.isogeny(T21)
-            phi2 = E.isogeny(S20)
-            assert T20 == T21 or T20.is_zero()
-            def isogeny(Ps):
-                P1, P2 = Ps
-                return phi1(P1), phi2(P2)
-        TS0 = isogeny([T0, S0])
-        TS1 = isogeny([T1, S1])
-        return [phi1.codomain(), phi2.codomain()], TS0[0], TS0[1], TS1[0], TS1[1], isogeny
+            # kernel is <((0,0),(0,0)), ((zeta2,0),(-zeta2,0))>, where zeta2^2 = -1
+            def isogeny(Rs):
+                R1, R2 = Rs
+                zeta2 = S1[0]
+                assert zeta2**2 == -1
+                def iota(R):
+                    return E([-R[0], zeta2*R[1], R[2]])
+                return R1 + iota(R2), iota(R1) + R2
 
-    # change T20 to (0, 0) in E
-    if T21[0] == 0:
-        T20, T21 = T21, T20
-        S20, S21 = S21, S20
-    elif not T20[0] == 0:
-        T20 = T20 + T21
-        S20 = S20 + S21
-
-    if S20[0] == 0:
-        if T21[0] == S21[0]:
-            def isogeny(Ps):
-                P1, P2 = Ps
-                return P1 - P2, P1 + P2
-        else:
-            def isogeny(Ps):
-                P1, P2 = Ps
-                zeta2 = T21[0]
-                def iota(P):
-                    return E([-P[0], zeta2*P[1], P[2]])
-                return P1 + iota(P2), iota(P1) + P2
-
-        TS0 = isogeny([T0, S0])
-        TS1 = isogeny([T1, S1])
-        return None, TS0[0], TS0[1], TS1[0], TS1[1], isogeny
+        imP = isogeny([P1, P2])
+        imQ = isogeny([Q1, Q2])
+        return None, imP[0], imP[1], imQ[0], imQ[1], isogeny
     else:
-        # change S21 to (0, 0) in E
-        if not S21[0] == 0:
-            T21 = T21 + T20
-            S21 = S21 + S20
+        # change S2 to (0, 0) in E
+        if not S2[0] == 0:
+            S1 += T1
+            S2 += T2
 
-        zeta2 = S20[0]
-        sign = F(-1)**(not T21[0] == zeta2)
+        zeta2 = T2[0]
+        assert zeta2**2 == -1
+        sign = F(-1)**(not S1[0] == zeta2)
         s1 = 2 * zeta2 / 3
         s1inv = -3 * zeta2 / 2
         t1 = 2 * sign * zeta2 / 3
@@ -173,23 +158,23 @@ def FromSpecialProdToDim2(T0, T1, S0, S1, e):
         a3_t = (-sign*zeta2 - s2) * s1inv
         h = s1 * (x**2 - a1_t) * (x**2 - a2_t) * (x**2 - a3_t)
 
-        def isogeny(Ps):
+        def isogeny(Rs):
             J = HyperellipticCurve(h).jacobian()
-            P1, P2 = Ps
+            R1, R2 = Rs
 
             if not P1.is_zero():
-                xP1, yP1 = P1.xy()
-                JP1 = J([s1 * x**2 + s2 - xP1, Rx(yP1 * s1inv)])
+                xR1, yR1 = R1.xy()
+                JR1 = J([s1 * x**2 + s2 - xR1, Rx(yR1 * s1inv)])
             if not P2.is_zero():
-                xP2, yP2 = P2.xy()
-                JP2 = J([(xP2 - t2) * x**2 - t1, yP2 * x**3 * t1inv])
-            if P1.is_zero():
-                return JP2
-            elif P2.is_zero():
-                return JP1
+                xR2, yR2 = R2.xy()
+                JR2 = J([(xR2 - t2) * x**2 - t1, yR2 * x**3 * t1inv])
+            if R1.is_zero():
+                return JR2
+            elif R2.is_zero():
+                return JR1
             else:
-                return JP1 + JP2
+                return JR1 + JR2
 
-        im0 = isogeny((T0, S0))
-        im1 = isogeny((T1, S1))
-        return h, im0[0], im0[1], im1[0], im1[1], isogeny
+        imP = isogeny((P1, P2))
+        imQ = isogeny((Q1, Q2))
+        return h, imP[0], imP[1], imQ[0], imQ[1], isogeny
